37168a23dd1d644430428bac02fda2a1a86906a2
sunniairfan <sunniairfan413@gmail.com>
2025-07-17 18:49:15 +0500

    Updated my app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,8 @@ dependencies {
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
+    implementation(libs.coil.compose)
+    implementation(libs.navigation.compose)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
