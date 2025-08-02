1579ce8c244ba9ab252edf4827f5ea910f804e2a
sunniairfan <sunniairfan413@gmail.com>
2025-08-02 19:45:43 +0500

    Updated my app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation(libs.androidx.ui)
     implementation(libs.coil.compose)
     implementation(libs.navigation.compose)
+    implementation(libs.okhttp)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
